4c893c83aaa2b7473687f750c0eb1c1279a5f810
Elam Day <elam.day.friedland@gmail.com>
2020-01-14 09:11:35 -0800

    peepee

--- My Python Scripts/Centrifyer.py
+++ My Python Scripts/Centrifyer.py
@@ -2,7 +2,12 @@ from textblob import TextBlob
 import nltk
 from textblob import Blobber
 from textblob.sentiments import NaiveBayesAnalyzer
+import twitter
 import re
+api = twitter.Api(consumer_key=["wBlchO1ZEiGnv1ly1wmVplNr5"],
+                  consumer_secret=["iyHcmJITDblWRf4Zj46r19UVbngnU8DoXkBoXCtchZpZ7kF6HT"],
+                  access_token_key=["1214224530341629952-RpXwF26MvZ0KEBJTcYzwEhKHgE4vQx"],
+                  access_token_secret=["GTa4it7WQ4pXnvu1klWlFnbMFfUSVVBw8gvu0sipWGeD7"])
 bl = Blobber(analyzer=NaiveBayesAnalyzer())
 at = input("what's the @?")
 print(at)
@@ -15,14 +20,14 @@ while True:
     print(at)
     confirm = input("does this look right? y/n")
 tweet = "standd in statement"
-tweetb = TextBlob(str("peepee poopoo"))
+tweetb = TextBlob(str(tweet))
 print(tweetb)
 print("first of all, let's fix this spelling, shall we?")
 spell = input("y/n")
 if spell == "y":
     corrected = tweetb.correct()
     print("before: " + tweet)
-print("after: " + corrected)
+print("after: " + str(corrected))
 
 
 
